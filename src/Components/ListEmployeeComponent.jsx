fc2039dc81a9a330f27fb108fb96124b99c0fd0a
Sai Chaitanya Raparti <chaitanya.raparti@gmail.com>
2021-08-13 13:41:02 +0530

    Initial Commit

---
+++ src/Components/ListEmployeeComponent.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class ListEmployeeComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <> 
+
+            </>
+        )
+    }
+}
+
+export default ListEmployeeComponent
