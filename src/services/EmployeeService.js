534aca7ce7fb4d0e095451b6f2e140a81aee1304
Sai Chaitanya Raparti <chaitanya.raparti@gmail.com>
2021-11-20 20:16:43 +0530

    changed API Base URL

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import axois from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1";
+const EMPLOYEE_API_BASE_URL = "http://localhost:8080/employee-management";
 
 class EmployeeService {
 
